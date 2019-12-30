34045cdda48b6da2a6c43d6fde94fcb9e594e6e5
Ebrahimrayatparvar <ebrahim.rayatparvar@hotmail.com>
2019-12-30 11:54:33 +0330

    init

---
+++ ConvertDate/ZcConvert.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertDate
+{
+    public class ZcConvert
+    {
+        public string MiladiToShamsi(DateTime dt)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            var shamsi = $"{pc.GetYear(dt)}/{pc.GetMonth(dt)}/{pc.GetDayOfMonth(dt)}";
+            return shamsi;
+        }
+
+        public int Pow(int dig)
+        {
+            return dig * dig;
+        }
+    }
+}
